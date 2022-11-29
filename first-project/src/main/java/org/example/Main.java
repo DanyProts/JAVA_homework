df18f68d6a1d71ab2fb146e3d81eb127b87601be
Арсений Чистяков <senyaaa@MacBook-Air-Arsenij.local>
2022-11-29 12:08:31 +0300

    #1 commit

--- first-project/src/main/java/org/example/Main.java
+++ first-project/src/main/java/org/example/Main.java
@@ -2,32 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int a = -1;
-        int b = 2;
-        double d = 2.2;
-        float f = 2.2f;
-        String s = "just-string";
-        boolean l = true;
-//        System.out.println(a + b);
-
-//        if (a > 0) {
-//            System.out.println(a);
-//        }else{
-//            System.out.println("a<=0");
-//        }
-//        int i = 0;
-//        while(i < 10){
-//            System.out.println(i);
-//            i++;
-//        }
-
-        Human human = new Human(30, 180);
-        Human human1 = new Human(29, 179);
-        Human baby1 = new Human(55);
-
-
-        System.out.println(baby1.olderThan18());
-        System.out.println(human.getAge());
-
+        int a = 9, b = -79, c = 8, d = 2929;
+        System.out.println(Math.max(Math.max(a , b), Math.max(c , d)));
     }
 }
