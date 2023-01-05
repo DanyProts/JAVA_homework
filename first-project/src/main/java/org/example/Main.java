8e612d5580db86ec6f290fa96cbee92e7715aa14
Vladimir Svetlakov <vova.svetl@mail.ru>
2023-01-05 18:59:50 +0300

    Запрограммирован поиск максимума из четырех чисел

--- first-project/src/main/java/org/example/Main.java
+++ first-project/src/main/java/org/example/Main.java
@@ -1,33 +1,24 @@
 package org.example;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int a = -1;
-        int b = 2;
-        double d = 2.2;
-        float f = 2.2f;
-        String s = "just-string";
-        boolean l = true;
-//        System.out.println(a + b);
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите четыре числа");
 
-//        if (a > 0) {
-//            System.out.println(a);
-//        }else{
-//            System.out.println("a<=0");
-//        }
-//        int i = 0;
-//        while(i < 10){
-//            System.out.println(i);
-//            i++;
-//        }
+        int n = 4;
+        int[] a = new int[n];
+        for (int i=0;i<n;i++){
+            a[i]=in.nextInt();
+        }
 
-        Human human = new Human(30, 180);
-        Human human1 = new Human(29, 179);
-        Human baby1 = new Human(55);
+        int max= a[0];
+        for (int i=1;i<n;i++){
+            if(max<a[i]){
+                max=a[i];
+            }
+        }
 
-
-        System.out.println(baby1.olderThan18());
-        System.out.println(human.getAge());
-
+        System.out.println("Максимум: "+max);
     }
 }
