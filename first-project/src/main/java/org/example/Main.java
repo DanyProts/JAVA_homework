8e2dd79edb95a7261036b7a36e1ee19d449edac9
andrsoin2089 <andrsoin@yandex.ru>
2024-12-07 21:55:31 +0300

    new main

--- first-project/src/main/java/org/example/Main.java
+++ first-project/src/main/java/org/example/Main.java
@@ -1,33 +1,26 @@
-package org.example;
+import java.util.Scanner;  // Import the Scanner class
 
 public class Main {
-    public static void main(String[] args) {
-        int a = -1;
-        int b = 2;
-        double d = 2.2;
-        float f = 2.2f;
-        String s = "just-string";
-        boolean l = true;
-//        System.out.println(a + b);
+    public static void main (String[] args){
+        int[] numbers = {0, 0, 0, 0};
 
-//        if (a > 0) {
-//            System.out.println(a);
-//        }else{
-//            System.out.println("a<=0");
-//        }
-//        int i = 0;
-//        while(i < 10){
-//            System.out.println(i);
-//            i++;
-//        }
-
-        Human human = new Human(30, 180);
-        Human human1 = new Human(29, 179);
-        Human baby1 = new Human(55);
+        Scanner in = new Scanner(System.in);
 
 
-        System.out.println(baby1.olderThan18());
-        System.out.println(human.getAge());
+        numbers[0] = in.nextInt();
+        numbers[1] = in.nextInt();
+        numbers[2] = in.nextInt();
+        numbers[3] = in.nextInt();
 
+        int mini = numbers[0];
+
+        for (int i = 0; i<4; i+=1){
+            //System.out.println(numbers[i]);
+            if (numbers[i] < mini){
+                mini = numbers[i];
+            } 
+        }
+
+        System.out.println(mini);
     }
 }
