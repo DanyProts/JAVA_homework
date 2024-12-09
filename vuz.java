565672689175e704e1431255ac2888b2add3e7e5
Makar <154985504+Sayflagdog@users.noreply.github.com>
2024-12-09 09:41:06 +0300

    Update and rename vuz.py to vuz.java
    переписал код с питона на джаву

---
+++ vuz.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MaxOfFour {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Числа");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+
+        int max = a; 
+        if (b > max) max = b;
+        if (c > max) max = c;
+        if (d > max) max = d;
+
+        System.out.println("max значение это: " + max);
+
+        scanner.close();
+    }
+}
